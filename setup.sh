3bc2d6272e1f9a2b6a8d3e61baae50f6625884ef
jstephencorey <55117445+jstephencorey@users.noreply.github.com>
2025-10-10 23:02:36 -0600

    change to not have double camera access

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ VIDEOS_DIR="/home/piuser/videos"
 
 echo "Creating video directories..."
 mkdir -p -m 755 "$VIDEOS_DIR/buffer" "$VIDEOS_DIR/clips"
+sudo chown -R piuser:piuser /home/piuser/videos
+mkfifo /tmp/motion.mjpeg
+sudo chown -R piuser:piuser /tmp/motion.mjpeg
 
 echo "Installing motion config..."
 sudo apt install -y motion ffmpeg
@@ -13,9 +16,7 @@ sudo systemctl enable motion
 
 echo "Installing services..."
 sudo cp camera.service /etc/systemd/system/camera.service
-sudo cp lowres_stream.service /etc/systemd/system/lowres_stream.service
 sudo systemctl daemon-reload
-sudo systemctl enable camera.service lowres_stream.service
 sudo systemctl start lowres_stream.service camera.service
 
 echo "✅ Setup complete. Recording should start at boot."
